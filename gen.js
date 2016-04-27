7c2a4b3586b8300eb613eb535abe60ad0ae9de98
James Hobin <hobinjk@mit.edu>
2016-04-27 12:42:15 -0400

    Regenerate database with more words

--- gen.js
+++ gen.js
@@ -1,34 +1,44 @@
-// Gen:
-/*
-words = {};
-for (var k of zn.textContent.split(/\W+/g)) {
-  k = k.toLowerCase();
-  if (!words[k]) words[k] = 0;
-  words[k] += 1;
-}
-var occWords = Object.keys(words).sort(function(a, b) {
-  return words[b] - words[a];
-})
-*/
+/*global thousand*/
 
-// var common = {};
-// thousand.forEach(function(word) {
-//   common[word.toLowerCase()] = true;
-// });
+// Gen:
+// var zn = document.querySelector('.zn-body__read-all');
+// var words = {};
+// for (var k of zn.textContent.split(/[ :?";,()\[\].]+/g)) {
+//   var key = k.toLowerCase();
+//   if (!words[key]) words[key] = {
+//     count: 0,
+//     text: k
+//   };
 // 
-// console.log(common);
-// 
-// var uncommon = [];
-// for (var word of words) {
-//   if (common[word]) {
-//     continue;
+//   words[key].count += 1;
+//   // Default to lower-case
+//   if (words[key].text != k) {
+//     words[key].text = key;
 //   }
-//   uncommon.push(word);
 // }
+// 
+// window.occWords = Object.keys(words).sort(function(a, b) {
+//   return words[b] - words[a];
+// });
+
+var common = {};
+thousand.forEach(function(word) {
+  common[word.toLowerCase()] = true;
+});
+
+console.log(common);
+
+var uncommon = [];
+for (var unword of words) {
+  if (common[unword]) {
+    continue;
+  }
+  uncommon.push(unword);
+}
 // document.body.innerHTML = uncommon.join('<br/>');
 
 var wordsDatabase = {};
-for (var word of words) {
+for (var word of uncommon) {
   if (!wordsDatabase[word.length]) {
     wordsDatabase[word.length] = [];
   }
